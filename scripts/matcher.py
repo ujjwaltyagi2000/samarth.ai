f33ddd5c6c13d675ecd59ae6530d8ed1aa432b36
ujjwaltyagi2000 <ujjwaltyagi28@gmail.com>
2025-04-20 15:59:20 +0530

    resolving conflicts

--- scripts/matcher.py
+++ scripts/matcher.py
@@ -1,6 +1,7 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+
 def calculate_match_score(resume_text, job_description):
     """Computes similarity score using TF-IDF & Cosine Similarity."""
     vectorizer = TfidfVectorizer()
--- scripts/text_processing.py
+++ scripts/text_processing.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import TreebankWordTokenizer
 
+
 def preprocess_text(text):
     tokenizer = TreebankWordTokenizer()
     tokens = tokenizer.tokenize(text.lower())  # Tokenize text
